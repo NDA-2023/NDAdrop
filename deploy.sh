eb4404e3e12fd8427599f5c4c1c5f10085cdb8e7
Maties Claesen <matiesclaesen@Matiess-MacBook-Pro.local>
2023-11-13 19:58:02 +0100

    Add deployment bash script, not that the gh-pages subbranch is deprecated

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cp -r dist/* ../../../NDA-2023.github.io/
+cp index.html error.html
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git add -A
+git commit -m "deploying"
+
+git push
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+
+cd -
