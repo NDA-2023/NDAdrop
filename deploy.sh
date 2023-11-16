9745f135dee8ef2560166da3e967677cc86ebab9
Maties Claesen <matiesclaesen@Matiess-MacBook-Pro.local>
2023-11-16 16:59:22 +0100

    updated deploy.sh -> parameter given -> only deploy backend

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,36 @@
 #!/usr/bin/env sh
 
-cd frontend/ndadrop
+if [ $# -eq 0 ]; then
+    cd frontend/ndadrop
 
-# abort on errors
-set -e
+    # abort on errors
+    set -e
 
-# build
-echo "========== building frontend =========="
-npm run build
+    # build
+    echo "========== building frontend =========="
+    npm run build
 
-# navigate into the build output directory
-echo "========== putting frontend in frontend repo =========="
-cp -r dist/* ../../../NDA-2023.github.io/
-cd ../../../NDA-2023.github.io/
-cp index.html error.html
+    # navigate into the build output directory
+    echo "========== putting frontend in frontend repo =========="
+    cp -r dist/* ../../../NDA-2023.github.io/
+    cd ../../../NDA-2023.github.io/
+    cp index.html error.html
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+    # if you are deploying to a custom domain
+    # echo 'www.example.com' > CNAME
 
-echo "========== pushing to frontend repo =========="
-git add -A
-git commit -m "deploying"
+    echo "========== pushing to frontend repo =========="
+    git add -A
+    git commit -m "deploying"
 
-git push
+    git push
 
-cd -
+    cd -
+    cd ../../
+else
+    echo "Argument provided: $1"
+    echo "only deploying backend"
+fi
 
 echo "========== putting backend in backend repo =========="
 cp -f backend/server.js ../NDAdrop-websocket-server/server.js
