c4d39b5a7cbda0013b7188528244070c4a57db8b
Maties Claesen <matiesclaesen@Matiess-MacBook-Pro.local>
2023-11-17 11:15:50 +0100

    update simple peer plugin

--- frontend/ndadrop/src/plugins/simplePeerPlugin.js
+++ frontend/ndadrop/src/plugins/simplePeerPlugin.js
@@ -2,7 +2,9 @@ export function importSimplePeer(isInitiator = false) {
   return import('https://cdn.jsdelivr.net/npm/simple-peer@9.11.1/simplepeer.min.js')
     .then(() => {
       // console.log(isInitiator);
-      const peerInstance = new SimplePeer({ initiator: isInitiator });
+      const peerInstance = new SimplePeer({ initiator: isInitiator, config: {
+        iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+      }, });
       return peerInstance;
     })
     .catch((error) => {
