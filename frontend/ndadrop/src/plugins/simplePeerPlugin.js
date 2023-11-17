ed1a41b7b829d2b90daafa78842db1b3b2c7e181
Maties Claesen <matiesclaesen@Matiess-MacBook-Pro.local>
2023-11-17 14:34:39 +0100

    add turn server

--- frontend/ndadrop/src/plugins/simplePeerPlugin.js
+++ frontend/ndadrop/src/plugins/simplePeerPlugin.js
@@ -2,9 +2,14 @@ export function importSimplePeer(isInitiator = false) {
   return import('https://cdn.jsdelivr.net/npm/simple-peer@9.11.1/simplepeer.min.js')
     .then(() => {
       // console.log(isInitiator);
-      const peerInstance = new SimplePeer({ initiator: isInitiator, config: {
-        iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
-      }, });
+      const peerInstance = new SimplePeer({
+        initiator: isInitiator, config: {
+          iceServers: [
+            { urls: 'stun:stun.l.google.com:19302' },
+            { urls: 'turn:main-bvxea6i-ivztmacy7gpi6.de-2.platformsh.site:3478', username: 'your-username', credential: 'your-password' },
+          ],
+        },
+      });
       return peerInstance;
     })
     .catch((error) => {
